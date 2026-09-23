c334db272c486b780bef0b354ce96db25de296d6
Etreus <jaimejalife@gmail.com>
2026-09-22 21:05:20 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -106,7 +106,7 @@ try:
             st.metric(
                 label=f"Último Precio ({tipo_petroleo})", 
                 value=f"${petroleo_ultimo:,.2f} USD",
-                delta=f"${variacion_petroleo:,.2f} USD "
+                delta=round(variacion_petroleo, 2)
             )
             
         with kpi_col2:
@@ -120,7 +120,7 @@ try:
             st.metric(
                 label=f"Último Precio ({tipo_cambio})", 
                 value=f"{divisa_ultima:,.4f} {simbolo_moneda}",
-                delta=f"{delta_divisa:,.4f} {simbolo_moneda} "
+                delta=round(delta_divisa, 4)
             )
 
        
